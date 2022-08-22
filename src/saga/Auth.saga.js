e1cd2be2c2955139f3492e8d2f601f8e768d6ca2
mitalitagadiya <mitalitagadiya@gamil.com>
2022-08-22 20:21:50 +0530

    auth

--- src/saga/Auth.saga.js
+++ src/saga/Auth.saga.js
@@ -7,6 +7,7 @@ function* Signup(action) {
    try {
       const user = yield call(SignUpapi, action.payload);
     //   yield put({type: "USER_FETCH_SUCCEEDED", user: user});
+    
    } catch (e) {
     //   yield put({type: "USER_FETCH_FAILED", message: e.message});
    }
