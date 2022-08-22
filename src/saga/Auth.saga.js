d1f4c8ea25fdb734b259e28045bd3a2a02c2caa5
mitalitagadiya <106071619+mitalitagadiya@users.noreply.github.com>
2022-08-22 02:44:05 -0700

    Update Auth.saga.js

--- src/saga/Auth.saga.js
+++ src/saga/Auth.saga.js
@@ -3,12 +3,15 @@ import { call, put, takeEvery, all } from 'redux-saga/effects'
 function* Signup(action) {
 
    try {
+      
       const user = yield call(SignUpapi, action.payload);
     //   yield put({type: "USER_FETCH_SUCCEEDED", user: user});
+      
    } catch (e) {
     //   yield put({type: "USER_FETCH_FAILED", message: e.message});
    }
 }
+
 
 function* watchSignUp() {
   yield takeEvery(ActionType.SIGN_UP, Signup);
@@ -16,6 +19,6 @@ function* watchSignUp() {
 
 export function* signUpSaga() {
     yield all([
-        watchSignUp
+        watchSignUp()
     ])
 }
